3066cdf340204de03d9aba96bdbd878623f9832c
huangjiajie <huangjiajie@shouqianba.com>
2022-06-17 20:38:27 +0800

    first commit

---
+++ src/test/java/com/jackerry/study/privilege/control/PrivilegeControlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jackerry.study.privilege.control;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrivilegeControlApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
